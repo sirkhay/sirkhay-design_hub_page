a5f7f76d3d029f323ac4e56de65490c2f860ff4d
Kehinde Bashorun <sirkennybashon@gmail.com>
2024-08-27 01:26:56 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/** @format */
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Button from "react-bootstrap/Button";
+
+function App() {
+  return (
+    <div className='App'>
+      <Button variant='primary'>Primary</Button>
+    </div>
+  );
+}
+
+export default App;
